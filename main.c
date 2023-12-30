1b37bc057dfb784d692f40961279515bb28d8a59
sellami mohamed ayoub <152229661+sellamiayoub@users.noreply.github.com>
2023-12-30 15:15:08 +0100

    test

--- main.c
+++ main.c
@@ -1,11 +1,17 @@
-#include"raylib.h"
+#include "raylib.h"
 void main(){
     InitWindow(800,800,"Project");
+    IsWindowResized();
+    MaximizeWindow();
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
-
+        ClearBackground(WHITE);
+        DrawText("IDK", 90, 90, 20, BLACK);
+        DrawEllipseLines(130, 100, 100, 70, BLACK);
         EndDrawing();
+        
     }
     CloseWindow();
 }
